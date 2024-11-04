a267eb4dbef40cb487be09a9c03a1e1bc81c6005
Lixx <i@lixx.cn>
2024-11-04 14:12:25 +0800

    update

---
+++ go/kubernetes/go.mod
@@ -0,0 +1,5 @@
+module example/kubernetes
+
+go 1.22.2
+
+require k8s.io/kubernetes v1.31.2 // indirect
---
+++ python/demo/generate_random_password.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+def generate_random_password(length=8):
+    # 定义字符集
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # 随机选择字符
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# 生成一个随机密码
+random_password = generate_random_password()
+print(f"生成的随机密码是: {random_password}")
